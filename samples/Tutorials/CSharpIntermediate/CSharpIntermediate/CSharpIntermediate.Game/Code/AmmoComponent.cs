37b73d3791c85669bd7f410df54b343b5fe8b85e
Aggror <aggror.jorn@gmail.com>
2020-05-09 20:52:29 +0200

    intermedia initial project

---
+++ samples/Tutorials/CSharpIntermediate/CSharpIntermediate/CSharpIntermediate.Game/Code/AmmoComponent.cs
@@ -0,0 +1,21 @@
+using Stride.Engine;
+
+namespace CSharpIntermediate.Code
+{
+    /// <summary>
+    /// This script is used in combination with the GettingAComponent.cs script 
+    /// </summary>
+    public class AmmoComponent : StartupScript
+    {
+        private readonly int clips = 4;
+        private readonly int bullets = 6;
+
+        public override void Start() { }
+
+        // This method return the total amount of ammo
+        public int GetTotalAmmo()
+        {
+            return bullets * clips;
+        }
+    }
+}
