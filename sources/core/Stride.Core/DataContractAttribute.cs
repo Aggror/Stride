110b9a94f5546712afdc213c86d5309b3304416e
BldgBlocks <122186550+BldgBlocks@users.noreply.github.com>
2023-09-24 14:42:17 -0700

    Spelling Update DataContractAttribute.cs and DataMemberMode.cs
    docs(api): Correct minor spelling and grammar.
    
    There were instances of the words 'property / field' 'property or field' and 'field/property' so I chose 'or' and standardized all occurrences.
    Some double spaces, or extra words.
    
    The DataMemberMode property summary uses more precise language to include structs. Unified the details to the DataContractAttribute.
    
    Summary 'Gets or sets' is unnecessary in summary. More to the point and maintainable to remove.
    
    REFACTOR: 'serialized' language is used. However, everywhere else 'store' and 'restore' are used. Can we unify this?

--- sources/core/Stride.Core/DataContractAttribute.cs
+++ sources/core/Stride.Core/DataContractAttribute.cs
@@ -5,7 +5,7 @@ using System;
 namespace Stride.Core
 {
     /// <summary>
-    /// Indicates that a class can be serialized.
+    /// Indicates that a class or structure can be serialized.
     /// </summary>
     [AttributeUsage(AttributeTargets.Delegate | AttributeTargets.Enum | AttributeTargets.Struct | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class DataContractAttribute : Attribute
@@ -29,7 +29,7 @@ namespace Stride.Core
         }
 
         /// <summary>
-        /// Gets or sets the alias name when serializing to a textual format.
+        /// The alias name when serializing to a textual format.
         /// </summary>
         /// <value>The alias name.</value>
         public string Alias
@@ -41,7 +41,7 @@ namespace Stride.Core
         }
 
         /// <summary>
-        /// Gets or sets a value indicating whether this <see cref="DataContractAttribute"/> is implicitly inherited by all its descendant classes.
+        /// Indicates whether this <see cref="DataContractAttribute"/> is implicitly inherited by all its descendant classes.
         /// </summary>
         /// <value><c>true</c> if inherited; otherwise, <c>false</c>.</value>
         public bool Inherited { get; set; }
--- sources/core/Stride.Core/DataMemberMode.cs
+++ sources/core/Stride.Core/DataMemberMode.cs
@@ -8,34 +8,34 @@ namespace Stride.Core
     public enum DataMemberMode
     {
         /// <summary>
-        /// Use the default mode depending on the type of the field/property.
+        /// Use the default mode depending on the type of the property or field.
         /// </summary>
         Default,
 
         /// <summary>
-        /// When restored, new object is created by using the parameters in
-        /// the YAML data and assigned to the property / field. When the
-        /// property / filed is writeable, this is the default.
+        /// When restored, a new object is created by using the parameters in
+        /// the stored data and assigned to the property or field. When the
+        /// property or field is writeable, this is the default.
         /// </summary>
         Assign,
 
         /// <summary>
-        ///  Only valid for a property / field that has a class or struct type.
+        ///  Only valid for a property or field that has a class or struct type.
         ///  When restored, instead of recreating the whole class or struct,
-        ///  the members are independently restored. When the property / field
+        ///  the members are independently restored. When the property or field
         ///  is not writeable this is the default.
         /// </summary>
         Content,
 
         /// <summary>
-        ///  Only valid for a property / field that has an  array type of a 
+        ///  Only valid for a property or field that has an array type of 
         ///  some value type. The content of the array is stored in a binary
         ///  format encoded in base64 style.
         /// </summary>
         Binary,
 
         /// <summary>
-        /// The property / field will not be stored.
+        /// The property or field will not be stored.
         /// </summary>
         Never,
     }
